fc4bac767fef36cb83df2c4d4d788743b713b66c
Jacques Schmitz Junior <juniorschmitz9@gmail.com>
2021-06-21 18:58:41 -0300

    refactor: modify the requests scenarios

--- cypress/integration/sending_requests_start.js
+++ cypress/integration/sending_requests_start.js
@@ -1,8 +1,28 @@
 /// <reference types="cypress" />
 
+beforeEach(() => {
+
+  cy.request({
+    method: 'POST',
+    url: '/api/reset'
+  })
+  
+})
+
 it('Sending requests', () => {
 
   cy
     .visit('/')
+
+
+  // useful for setting the application in a specific state, API calls and etc
+  cy
+    .request({
+      method: 'POST',
+      url: '/api/boards',
+      body: {
+        name: 'board created by .request() command'
+      }
+    })
 
 });
